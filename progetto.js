cca8fa2b48e30ed5c7fe066ca675bf5284a68839
FEDERICO <federico.saggiro@icloud.com>
2024-11-08 09:39:52 +0100

    si

--- progetto.js
+++ progetto.js
@@ -20,10 +20,12 @@ let places = [
 let zoom = 12;
 let maxZoom = 19;
 let map = L.map('map').setView(places[0].coords, zoom);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: maxZoom,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 places.forEach((place) => {
     const marker = L.marker(place.coords).addTo(map);
     marker.bindPopup(`<b>${place.name}</b>`);
